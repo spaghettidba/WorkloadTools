8d2ae077f72f0e1beb4295523557a1fc870d8536
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2018-11-22 19:24:11 +0100

    Added logging and skipping of malformed datapoints

--- WorkloadViewer/Model/WorkloadAnalysis.cs
+++ WorkloadViewer/Model/WorkloadAnalysis.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.SqlClient;
@@ -12,6 +13,8 @@ namespace WorkloadViewer.Model
 {
     public class WorkloadAnalysis
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public ObservableCollection<WorkloadAnalysisPoint> Points { get; set; }
 
         public string Name { get; set; }
@@ -53,34 +56,41 @@ namespace WorkloadViewer.Model
                         Points = new ObservableCollection<WorkloadAnalysisPoint>();
                         while (rdr.Read())
                         {
-                            WorkloadAnalysisPoint point = new WorkloadAnalysisPoint()
+                            try
                             {
-                                OffsetMinutes = rdr.GetInt32(rdr.GetOrdinal("offset_minutes")),
-                                DurationMinutes = rdr.GetInt32(rdr.GetOrdinal("duration_minutes")),
-                                NormalizedQuery = NormalizedQueries[rdr.GetInt64(rdr.GetOrdinal("sql_hash"))],
-                                ApplicationName = rdr.GetString(rdr.GetOrdinal("application_name")),
-                                DatabaseName = rdr.GetString(rdr.GetOrdinal("database_name")),
-                                LoginName = rdr.GetString(rdr.GetOrdinal("login_name")),
-                                HostName = rdr.GetString(rdr.GetOrdinal("host_name")),
-                                AvgCpuMs = rdr.GetInt64(rdr.GetOrdinal("avg_cpu_ms")),
-                                MinCpuMs = rdr.GetInt64(rdr.GetOrdinal("min_cpu_ms")),
-                                MaxCpuMs = rdr.GetInt64(rdr.GetOrdinal("max_cpu_ms")),
-                                SumCpuMs = rdr.GetInt64(rdr.GetOrdinal("sum_cpu_ms")),
-                                AvgReads = rdr.GetInt64(rdr.GetOrdinal("avg_reads")),
-                                MinReads = rdr.GetInt64(rdr.GetOrdinal("min_reads")),
-                                MaxReads = rdr.GetInt64(rdr.GetOrdinal("max_reads")),
-                                SumReads = rdr.GetInt64(rdr.GetOrdinal("sum_reads")),
-                                AvgWrites = rdr.GetInt64(rdr.GetOrdinal("avg_writes")),
-                                MinWrites = rdr.GetInt64(rdr.GetOrdinal("min_writes")),
-                                MaxWrites = rdr.GetInt64(rdr.GetOrdinal("max_writes")),
-                                SumWrites = rdr.GetInt64(rdr.GetOrdinal("sum_writes")),
-                                AvgDurationMs = rdr.GetInt64(rdr.GetOrdinal("avg_duration_ms")),
-                                MinDurationMs = rdr.GetInt64(rdr.GetOrdinal("min_duration_ms")),
-                                MaxDurationMs = rdr.GetInt64(rdr.GetOrdinal("max_duration_ms")),
-                                SumDurationMs = rdr.GetInt64(rdr.GetOrdinal("sum_duration_ms")),
-                                ExecutionCount = rdr.GetInt64(rdr.GetOrdinal("execution_count"))
-                            };
-                            Points.Add(point);
+                                WorkloadAnalysisPoint point = new WorkloadAnalysisPoint()
+                                {
+                                    OffsetMinutes = rdr.GetInt32(rdr.GetOrdinal("offset_minutes")),
+                                    DurationMinutes = rdr.GetInt32(rdr.GetOrdinal("duration_minutes")),
+                                    NormalizedQuery = NormalizedQueries[rdr.GetInt64(rdr.GetOrdinal("sql_hash"))],
+                                    ApplicationName = rdr.GetString(rdr.GetOrdinal("application_name")),
+                                    DatabaseName = rdr.GetString(rdr.GetOrdinal("database_name")),
+                                    LoginName = rdr.GetString(rdr.GetOrdinal("login_name")),
+                                    HostName = rdr.GetString(rdr.GetOrdinal("host_name")),
+                                    AvgCpuMs = rdr.GetInt64(rdr.GetOrdinal("avg_cpu_ms")),
+                                    MinCpuMs = rdr.GetInt64(rdr.GetOrdinal("min_cpu_ms")),
+                                    MaxCpuMs = rdr.GetInt64(rdr.GetOrdinal("max_cpu_ms")),
+                                    SumCpuMs = rdr.GetInt64(rdr.GetOrdinal("sum_cpu_ms")),
+                                    AvgReads = rdr.GetInt64(rdr.GetOrdinal("avg_reads")),
+                                    MinReads = rdr.GetInt64(rdr.GetOrdinal("min_reads")),
+                                    MaxReads = rdr.GetInt64(rdr.GetOrdinal("max_reads")),
+                                    SumReads = rdr.GetInt64(rdr.GetOrdinal("sum_reads")),
+                                    AvgWrites = rdr.GetInt64(rdr.GetOrdinal("avg_writes")),
+                                    MinWrites = rdr.GetInt64(rdr.GetOrdinal("min_writes")),
+                                    MaxWrites = rdr.GetInt64(rdr.GetOrdinal("max_writes")),
+                                    SumWrites = rdr.GetInt64(rdr.GetOrdinal("sum_writes")),
+                                    AvgDurationMs = rdr.GetInt64(rdr.GetOrdinal("avg_duration_ms")),
+                                    MinDurationMs = rdr.GetInt64(rdr.GetOrdinal("min_duration_ms")),
+                                    MaxDurationMs = rdr.GetInt64(rdr.GetOrdinal("max_duration_ms")),
+                                    SumDurationMs = rdr.GetInt64(rdr.GetOrdinal("sum_duration_ms")),
+                                    ExecutionCount = rdr.GetInt64(rdr.GetOrdinal("execution_count"))
+                                };
+                                Points.Add(point);
+                            }
+                            catch(Exception e)
+                            {
+                                logger.Warn($"Skipping invalid datapoint at {rdr.GetInt32(rdr.GetOrdinal("offset_minutes"))} because of Exception: {e.StackTrace}");
+                            }
                         }
                     }
                 }
