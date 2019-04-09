818a68cd949dd136f33163d5a3125840ff46057d
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2019-04-09 13:05:28 +0200

    Added try/cath inside loop for reading events

--- WorkloadTools/Listener/ExtendedEvents/StreamXEventDataReader.cs
+++ WorkloadTools/Listener/ExtendedEvents/StreamXEventDataReader.cs
@@ -45,101 +45,108 @@ namespace WorkloadTools.Listener.ExtendedEvents
                 {
                     PublishedEvent evt = eventsEnumerator.Current;
                     ExecutionWorkloadEvent evnt = new ExecutionWorkloadEvent();
-
-                    string commandText = String.Empty;
-                    if (evt.Name == "rpc_completed")
-                    {
-                        commandText = (string)TryGetValue(evt, FieldType.Field, "statement");
-                        evnt.Type = WorkloadEvent.EventType.RPCCompleted;
-                    }
-                    else if (evt.Name == "sql_batch_completed")
-                    {
-                        commandText = (string)TryGetValue(evt, FieldType.Field, "batch_text");
-                        evnt.Type = WorkloadEvent.EventType.BatchCompleted;
-                    }
-                    else if (evt.Name == "attention")
-                    {
-                        object value = TryGetValue(evt, FieldType.Action, "sql_text");
-                        try
-                        {
-                            if (value is string)
-                                commandText = (string)value;
-                            else if (value is byte[])
-                                commandText = Encoding.Unicode.GetString((byte[])value);
-                            else throw new ArgumentException("Argument is of the wrong type");
-                        }
-                        catch(Exception e)
-                        {
-                            logger.Error(e,$"Unable to extract sql_text from attention event. Value is of type ${value.GetType().FullName}");
-
-                        }
-                        evnt.Type = WorkloadEvent.EventType.Timeout;
-                    }
-					else if (evt.Name == "user_event")
-					{
-						int num = (int)TryGetValue(evt, FieldType.Field, "event_id");
-						if(num == 83)
-						{
-							commandText = (string)TryGetValue(evt, FieldType.Field, "user_data");
-							evnt.Type = WorkloadEvent.EventType.Error;
-						}
-					}
-					else
-                    {
-                        evnt.Type = WorkloadEvent.EventType.Unknown;
-                        continue;
-                    }
-
                     try
                     {
-                        evnt.ApplicationName = (string)TryGetValue(evt, FieldType.Action, "client_app_name");
-                        evnt.DatabaseName = (string)TryGetValue(evt, FieldType.Action, "database_name");
-                        evnt.HostName = (string)TryGetValue(evt, FieldType.Action, "client_hostname");
-                        evnt.LoginName = (string)TryGetValue(evt, FieldType.Action, "server_principal_name");
-                        object oSession = TryGetValue(evt, FieldType.Action, "session_id");
-                        if (oSession != null)
-                            evnt.SPID = Convert.ToInt32(oSession);
-                        if (commandText != null)
-                            evnt.Text = commandText;
 
-
-                        evnt.StartTime = evt.Timestamp.LocalDateTime;
-
-						if (evnt.Type == WorkloadEvent.EventType.Error)
-						{
-							// do nothing
-						}
-                        else if (evnt.Type == WorkloadEvent.EventType.Timeout)
+                        string commandText = String.Empty;
+                        if (evt.Name == "rpc_completed")
                         {
-                            evnt.Duration = Convert.ToInt64(evt.Fields["duration"].Value);
-                            evnt.CPU = Convert.ToInt64(evnt.Duration);
+                            commandText = (string)TryGetValue(evt, FieldType.Field, "statement");
+                            evnt.Type = WorkloadEvent.EventType.RPCCompleted;
+                        }
+                        else if (evt.Name == "sql_batch_completed")
+                        {
+                            commandText = (string)TryGetValue(evt, FieldType.Field, "batch_text");
+                            evnt.Type = WorkloadEvent.EventType.BatchCompleted;
+                        }
+                        else if (evt.Name == "attention")
+                        {
+                            object value = TryGetValue(evt, FieldType.Action, "sql_text");
+                            try
+                            {
+                                if (value is string)
+                                    commandText = (string)value;
+                                else if (value is byte[])
+                                    commandText = Encoding.Unicode.GetString((byte[])value);
+                                else throw new ArgumentException("Argument is of the wrong type");
+                            }
+                            catch (Exception e)
+                            {
+                                logger.Error(e, $"Unable to extract sql_text from attention event. Value is of type ${value.GetType().FullName}");
+
+                            }
+                            evnt.Type = WorkloadEvent.EventType.Timeout;
+                        }
+                        else if (evt.Name == "user_event")
+                        {
+                            int num = (int)TryGetValue(evt, FieldType.Field, "event_id");
+                            if (num == 83)
+                            {
+                                commandText = (string)TryGetValue(evt, FieldType.Field, "user_data");
+                                evnt.Type = WorkloadEvent.EventType.Error;
+                            }
                         }
                         else
                         {
-                            evnt.Reads = Convert.ToInt64(evt.Fields["logical_reads"].Value);
-                            evnt.Writes = Convert.ToInt64(evt.Fields["writes"].Value);
-                            evnt.CPU = Convert.ToInt64(evt.Fields["cpu_time"].Value);
-                            evnt.Duration = Convert.ToInt64(evt.Fields["duration"].Value);
+                            evnt.Type = WorkloadEvent.EventType.Unknown;
+                            continue;
                         }
 
+                        try
+                        {
+                            evnt.ApplicationName = (string)TryGetValue(evt, FieldType.Action, "client_app_name");
+                            evnt.DatabaseName = (string)TryGetValue(evt, FieldType.Action, "database_name");
+                            evnt.HostName = (string)TryGetValue(evt, FieldType.Action, "client_hostname");
+                            evnt.LoginName = (string)TryGetValue(evt, FieldType.Action, "server_principal_name");
+                            object oSession = TryGetValue(evt, FieldType.Action, "session_id");
+                            if (oSession != null)
+                                evnt.SPID = Convert.ToInt32(oSession);
+                            if (commandText != null)
+                                evnt.Text = commandText;
+
+
+                            evnt.StartTime = evt.Timestamp.LocalDateTime;
+
+                            if (evnt.Type == WorkloadEvent.EventType.Error)
+                            {
+                                // do nothing
+                            }
+                            else if (evnt.Type == WorkloadEvent.EventType.Timeout)
+                            {
+                                evnt.Duration = Convert.ToInt64(evt.Fields["duration"].Value);
+                                evnt.CPU = Convert.ToInt64(evnt.Duration);
+                            }
+                            else
+                            {
+                                evnt.Reads = Convert.ToInt64(evt.Fields["logical_reads"].Value);
+                                evnt.Writes = Convert.ToInt64(evt.Fields["writes"].Value);
+                                evnt.CPU = Convert.ToInt64(evt.Fields["cpu_time"].Value);
+                                evnt.Duration = Convert.ToInt64(evt.Fields["duration"].Value);
+                            }
+
+                        }
+                        catch (Exception e)
+                        {
+                            logger.Error(e, "Error converting XE data from the stream.");
+                            throw;
+                        }
+
+                        if (evnt.Type <= WorkloadEvent.EventType.BatchCompleted)
+                        {
+                            if (transformer.Skip(evnt.Text))
+                                continue;
+
+                            evnt.Text = transformer.Transform(evnt.Text);
+                        }
+
+                        Events.Enqueue(evnt);
+
+                        EventCount++;
                     }
-                    catch (Exception e)
+                    catch (Exception ex)
                     {
-                        logger.Error(e, "Error converting XE data from the stream.");
-                        throw;
+                        logger.Error(ex, "Error converting XE data from the stream.");
                     }
-
-					if(evnt.Type <= WorkloadEvent.EventType.BatchCompleted)
-					{
-						if (transformer.Skip(evnt.Text))
-							continue;
-
-						evnt.Text = transformer.Transform(evnt.Text);
-					}
-
-                    Events.Enqueue(evnt);
-
-                    EventCount++;
                 }
             }
         }
