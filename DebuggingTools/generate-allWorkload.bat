90786397bb4764719ebb3de1d2581d5d2c773154
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2020-01-20 19:28:40 +0100

    Fixed debug scripts

--- DebuggingTools/generate-allWorkload.bat
+++ DebuggingTools/generate-allWorkload.bat
@@ -1,14 +1,5 @@
 del "c:\temp\workloadtools\debug\capture.sqlite"
-sqlcmd -S(local)\sqlexpress2016 -dlsctbenchmark -Q"TRUNCATE TABLE test.Applications"
-sqlcmd -S(local)\sqlexpress2016 -dlsctbenchmark -Q"TRUNCATE TABLE test.Databases"
-sqlcmd -S(local)\sqlexpress2016 -dlsctbenchmark -Q"TRUNCATE TABLE test.Errors"
-sqlcmd -S(local)\sqlexpress2016 -dlsctbenchmark -Q"TRUNCATE TABLE test.Hosts"
-sqlcmd -S(local)\sqlexpress2016 -dlsctbenchmark -Q"TRUNCATE TABLE test.Intervals"
-sqlcmd -S(local)\sqlexpress2016 -dlsctbenchmark -Q"TRUNCATE TABLE test.Logins"
-sqlcmd -S(local)\sqlexpress2016 -dlsctbenchmark -Q"TRUNCATE TABLE test.NormalizedQueries"
-sqlcmd -S(local)\sqlexpress2016 -dlsctbenchmark -Q"TRUNCATE TABLE test.WaitStats"
-sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.WorkloadDetails"
-sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE benchmark"
+sqlcmd -S(local) -dbenchmark -Q"TRUNCATE TABLE dbo.benchmark"
 start "" powershell -File .\generate-workload.ps1 -start 0
 start "" powershell -File .\generate-workload.ps1 -start 1000000
 start "" powershell -File .\generate-workload.ps1 -start 2000000
--- DebuggingTools/start-replay.bat
+++ DebuggingTools/start-replay.bat
@@ -1,4 +1,13 @@
 echo %time%
 sqlcmd -S(local) -dbenchmark -Q"TRUNCATE TABLE benchmark"
+sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.Applications"
+sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.Databases"
+sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.Errors"
+sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.Hosts"
+sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.Intervals"
+sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.Logins"
+sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.NormalizedQueries"
+sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.WaitStats"
+sqlcmd -S(local) -dbenchmark_analysis -Q"TRUNCATE TABLE test.WorkloadDetails"
 "c:\program files\workloadtools\sqlworkload.exe" --File "%CD%\replay.json"
 echo %time%
