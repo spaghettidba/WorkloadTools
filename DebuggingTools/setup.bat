3e5bbd55a02dcbad4932abd169e666788bda6739
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2020-01-20 19:18:30 +0100

    Create folders

--- DebuggingTools/setup.bat
+++ DebuggingTools/setup.bat
@@ -1,3 +1,7 @@
 sqlcmd -S(local) -Q"CREATE DATABASE benchmark"
 sqlcmd -S(local) -dbenchmark -Q"CREATE TABLE dbo.benchmark ( i int NULL )"
 sqlcmd -S(local) -Q"CREATE DATABASE benchmark_analysis"
+
+mkdir c:\temp
+mkdir c:\temp\workloadtools
+mkdir c:\temp\workloadtools\debug
