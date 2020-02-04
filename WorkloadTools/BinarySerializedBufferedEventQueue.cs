17e2607c7e983012250d7b3b0fb1dc5ee1336e09
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2020-02-04 23:59:03 +0100

    Improved temp file naming

--- WorkloadTools/BinarySerializedBufferedEventQueue.cs
+++ WorkloadTools/BinarySerializedBufferedEventQueue.cs
@@ -16,23 +16,24 @@ namespace WorkloadTools
 
         private int _minFile, _maxFile;
 
-        private static string FILE_NAME_UNIQUIFIER = DateTime.Now.ToString("yyyyMMddHHmm") + "_" 
-            + ("000000000" + System.Diagnostics.Process.GetCurrentProcess().Id).Right(9) + "_";
+        private string file_name_uniquifier = "";
 
         private BinaryFormatter _formatter = new BinaryFormatter();
 
         public BinarySerializedBufferedEventQueue() : base()
         {
+            file_name_uniquifier = DateTime.Now.ToString("yyyyMMddHHmm") + "_" + ("000000000" + (Environment.TickCount & Int32.MaxValue)).Right(9) + "_";
             baseFolder = Path.Combine(Path.Combine(System.IO.Path.GetTempPath(), "WorkloadTools"), "SerializedEventQueue");
             System.IO.Directory.CreateDirectory(baseFolder);
             _minFile = 0;
             _maxFile = 0;
+
         }
 
         protected override WorkloadEvent[] ReadEvents(int count)
         {
             WorkloadEvent[] result = null;
-            string destFile = Path.Combine(baseFolder, FILE_NAME_UNIQUIFIER + ("000000000" + _minFile).Right(9) + ".cache");
+            string destFile = Path.Combine(baseFolder, file_name_uniquifier + ("000000000" + _minFile).Right(9) + ".cache");
             
             using (System.IO.FileStream fileStream = new System.IO.FileStream(destFile, System.IO.FileMode.Open))
             using (BufferedStream bufferedStream = new BufferedStream(fileStream))
@@ -52,7 +53,7 @@ namespace WorkloadTools
 
         protected override void WriteEvents(WorkloadEvent[] events)
         {
-            string destFile = Path.Combine(baseFolder, FILE_NAME_UNIQUIFIER);
+            string destFile = Path.Combine(baseFolder, file_name_uniquifier);
             // c# does not have a String.Right method, so I created
             // an extension for it. Crazy, right?
             destFile += ("000000000" + _maxFile).Right(9) + ".cache";
@@ -76,7 +77,7 @@ namespace WorkloadTools
             // delete all pending files
             for (int i=_minFile; i<=_maxFile; i++)
             {
-                string destFile = Path.Combine(baseFolder, FILE_NAME_UNIQUIFIER);
+                string destFile = Path.Combine(baseFolder, file_name_uniquifier);
                 destFile += ("000000000" + i).Right(9) + ".cache";
                 if (File.Exists(destFile))
                 {
