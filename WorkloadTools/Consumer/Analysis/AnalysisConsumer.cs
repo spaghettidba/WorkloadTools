6effae01813bcefb121be02c9b6740f88b71681b
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2020-05-17 19:16:14 +0200

    Force UploadIntervalSeconds to be in minutes

--- WorkloadTools/Consumer/Analysis/AnalysisConsumer.cs
+++ WorkloadTools/Consumer/Analysis/AnalysisConsumer.cs
@@ -12,8 +12,30 @@ namespace WorkloadTools.Consumer.Analysis
         private static Logger logger = LogManager.GetCurrentClassLogger();
         private WorkloadAnalyzer analyzer;
 
+        private int _uploadIntervalSeconds;
+
         public SqlConnectionInfo ConnectionInfo { get; set; }
-        public int UploadIntervalSeconds { get; set; }
+        public int UploadIntervalSeconds {
+            get
+            {
+                return _uploadIntervalSeconds;
+            }
+            set
+            {
+                if(value % 60 != 0)
+                {
+                    throw new ArgumentOutOfRangeException("UploadIntervalSeconds must be an exact multiple of 60");
+                }
+                _uploadIntervalSeconds = value;
+            }
+        }
+
+        public int UploadIntervalMinutes
+        {
+            get { return _uploadIntervalSeconds / 60; }
+            set { _uploadIntervalSeconds = value * 60; }
+        }
+
 		public int MaximumWriteRetries { get; set; } = 5;
 
 		public bool SqlNormalizerTruncateTo4000 { get; set; }
