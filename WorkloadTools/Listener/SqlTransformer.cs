33de01b6ad6a53925fe933ea3742deda8ff9006d
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2020-11-10 13:40:49 +0100

    Added check for decimal strings longer than 38 digits

--- WorkloadTools/Listener/SqlTransformer.cs
+++ WorkloadTools/Listener/SqlTransformer.cs
@@ -16,6 +16,13 @@ namespace WorkloadTools.Listener
         private static Regex _preppedSqlStatement = new Regex("^(')(?<statement>((?!\\1).|\\1{2})*)\\1", RegexOptions.Compiled | RegexOptions.Singleline);
         private static Regex _doubleApostrophe = new Regex("('')(?<string>.*?)('')", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace | RegexOptions.CultureInvariant);
 
+        private static MatchEvaluator decimal38Evaluator = new MatchEvaluator(MakeFloat);
+
+        private static string MakeFloat(Match match)
+        {
+            return match.Value + "E0";
+        }
+
 
         public string Transform(string command)
         {
@@ -43,6 +50,18 @@ namespace WorkloadTools.Listener
                 if (!command.EndsWith("EXEC sp_cursorunprepare @p1;"))
                     command += " ; EXEC sp_cursorunprepare @p1;";
             }
+
+
+            // trim numbers with precision > 38
+            // rpc_completed events may return float parameters
+            // as long numeric strings that exceed the maximum decimal
+            // precision of 38. 
+            // Any decimal numeric string in T-SQL is interpreted as decimal,
+            // unless it ends with "E0", which designates a float literal. 
+            // Any decimal numeric string longer than 38 characters needs to
+            // be appended "E0" to be treated as float.
+            command = Regex.Replace(command, @"[0-9\.]{38,}", decimal38Evaluator);
+
             return command;
         }
 
