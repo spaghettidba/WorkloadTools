bbbe646034cdbdae258d1227d5015e3a1ebddcc2
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2022-09-20 17:52:23 +0200

    Fix for WorkloadSummary

--- WorkloadTools/Consumer/Analysis/WorkloadAnalyzer.cs
+++ WorkloadTools/Consumer/Analysis/WorkloadAnalyzer.cs
@@ -645,9 +645,10 @@ namespace WorkloadTools.Consumer.Analysis
                     sum_writes += T.sum_writes,
                     min_duration_us = CASE WHEN T.min_duration_us < WS.min_duration_us THEN T.min_duration_us ELSE WS.min_duration_us END,
                     max_duration_us = CASE WHEN T.max_duration_us > WS.max_duration_us THEN T.max_duration_us ELSE WS.max_duration_us END,
+                    sum_duration_us += T.sum_duration_us,
                     min_execution_date = CASE WHEN T.min_execution_date < WS.min_execution_date THEN T.min_execution_date ELSE WS.min_execution_date END,
                     max_execution_date = CASE WHEN T.max_execution_date > WS.max_execution_date THEN T.max_execution_date ELSE WS.max_execution_date END,
-                    sum_duration_us += T.sum_duration_us
+                    execution_count += execution_count
                 FROM [{ConnectionInfo.SchemaName}].WorkloadSummary AS WS
                 INNER JOIN #WorkloadSummary AS T
                     ON  T.application_id = WS.application_id
