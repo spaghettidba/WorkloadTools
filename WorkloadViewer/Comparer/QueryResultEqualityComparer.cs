348eb76d32ced6cd9d9b306f95e8a6f0978a5ec0
Silent1984 <JensNintemann@gmx.de>
2023-11-23 20:15:39 +0100

    Fix #115 WorkloadViewer shows queries twice

---
+++ WorkloadViewer/Comparer/QueryResultEqualityComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using WorkloadViewer.ViewModel;
+
+public class QueryResultEqualityComparer : IEqualityComparer<QueryResult>
+{
+    public bool Equals(QueryResult x, QueryResult y)
+    {
+        return x?.query_hash == y?.query_hash;
+    }
+
+    public int GetHashCode(QueryResult obj)
+    {
+        return obj?.query_hash.GetHashCode() ?? 0;
+    }
+}
